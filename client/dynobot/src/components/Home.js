46a6f9a2d08a4dc79f5d97fc5c0007c9ae437e2f
Prince <pmdhamecha673@gmail.com>
2023-04-29 15:02:57 +0530

    server setup

--- client/dynobot/src/components/Home.js
+++ client/dynobot/src/components/Home.js
@@ -2,26 +2,33 @@ import React from "react";
 import logo from "../utils/52.jpg";
 const Home = () => {
   return (
-    <div className="d-flex m-auto h-100">
-      <div className="m-auto">
-        <div
-          class="card text-white bg-primary mb-3"
-          style={{ maxWidth: "20rem" }}
-        >
-          <div class="card-header">Header</div>
-          <div class="card-body">
-            <img className="w-100 h-auto" src={logo}></img>
-          </div>
+    <div className="">
+      <div className="d-flex m-auto h-100">
+        <div className="col-2">
+          <p>Header</p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima
+            temporibus dolor unde incidunt et, quis eligendi soluta provident
+            cupiditate cumque quae quibusdam labore delectus fugiat molestiae
+            fugit minus assumenda dolore dignissimos sint fuga numquam
+            voluptates. Tenetur ratione ea obcaecati deserunt libero.
+            Temporibus, corrupti ipsa! Rem illo accusamus harum soluta
+            quibusdam? Repellendus atque.
+          </p>
         </div>
-        <div
-          class="card text-white bg-primary mb-3"
-          style={{ maxWidth: "30rem" }}
-        >
-          <div class="card-header">Header</div>
-          <div class="card-body">
-            <img className="w-100 h-auto" src={logo}></img>
-          </div>
+        <div className="col-2">
+          <p>Header</p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima
+            temporibus dolor unde incidunt et, quis eligendi soluta provident
+            cupiditate cumque quae quibusdam labore delectus fugiat molestiae
+            fugit minus assumenda dolore dignissimos sint fuga numquam
+            voluptates. Tenetur ratione ea obcaecati deserunt libero.
+            Temporibus, corrupti ipsa! Rem illo accusamus harum soluta
+            quibusdam? Repellendus atque.
+          </p>
         </div>
+        <div className="col-4"></div>
       </div>
     </div>
   );
---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import path from "path";
+const app = express();
+
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+import dotenv from "dotenv";
+dotenv.config();
+
+import "express-async-errors";
+import connectDB from "./db/connect.js";
+import cors from "cors";
+
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
