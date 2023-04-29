453c15c5e84b974eb4e8750ae154d55c97a27438
Vashishth Patel <vashishthchaudhary48@gmail.com>
2023-04-29 19:55:36 +0530

    testing api done & bot replying successfully

--- bot/events/comments.js
+++ bot/events/comments.js
@@ -1,41 +1,59 @@
-
 const reactOnIssueCommentCreate = async (context) => {
-    var content;
-    var body = context.payload.comment.body;
-    if (body.toLowerCase().includes("thank")) {
-      content = "heart";
-    } else if (body.toLowerCase().includes("good")) {
-      content = "rocket";
-    } else if (body.toLowerCase().includes("funny")) {
-      content = "laugh";
-    } else {
-      return;
+  var content;
+  var body = context.payload.comment.body;
+  if (body.toLowerCase().includes("thank")) {
+    content = "heart";
+  } else if (body.toLowerCase().includes("good")) {
+    content = "rocket";
+  } else if (body.toLowerCase().includes("funny")) {
+    content = "laugh";
+  } else {
+    return;
+  }
+  var repo = context.payload.repository.name;
+  var comment_id = context.payload.comment.id;
+  var owner = context.payload.repository.owner.login;
+  return context.octokit.reactions.createForIssueComment({
+    owner,
+    repo,
+    comment_id,
+    content,
+  });
+};
+
+const reactOnIssueCommentEdit = async (context) => {
+  var content = "eyes";
+  var repo = context.payload.repository.name;
+  var comment_id = context.payload.comment.id;
+  var owner = context.payload.repository.owner.login;
+  return context.octokit.reactions.createForIssueComment({
+    owner,
+    repo,
+    comment_id,
+    content,
+  });
+};
+
+const chatWithComment = async (context, userData) => {
+  var body = context.payload.comment.body;
+
+  var actions = userData.actions
+
+  for(var i = 0; i  < actions.length; i++){
+    var obj = actions[i];
+    if(obj.key === body){
+      const params = context.issue({
+        body: obj.value,
+      });
+      return await context.octokit.issues.createComment(params);
     }
-    var repo = context.payload.repository.name;
-    var comment_id = context.payload.comment.id;
-    var owner = context.payload.repository.owner.login;
-    return context.octokit.reactions.createForIssueComment({
-      owner,
-      repo,
-      comment_id,
-      content,
-    });
-  };
-  
-  const reactOnIssueCommentEdit = async (context) => {
-    var content = "eyes";
-    var repo = context.payload.repository.name;
-    var comment_id = context.payload.comment.id;
-    var owner = context.payload.repository.owner.login;
-    return context.octokit.reactions.createForIssueComment({
-      owner,
-      repo,
-      comment_id,
-      content,
-    });
-  };
-  
-  module.exports = {
-    reactOnIssueCommentCreate,
-    reactOnIssueCommentEdit,
-  };
+  }
+  consoler.log("Nothing for party 🎉")
+  return;
+};
+
+module.exports = {
+  reactOnIssueCommentCreate,
+  reactOnIssueCommentEdit,
+  chatWithComment,
+};
--- bot/middlewares/authenticate.js
+++ bot/middlewares/authenticate.js
@@ -1,22 +1,30 @@
-module.export = async function getUser(context) {
+require('dotenv').config()
+
+async function getUser(context) {
   console.log("Inside any things");
   var responseData;
   console.log(context.payload.repository.owner.login);
   try {
+    const SERVER_URL = process.env["SERVER_URL"]
     const response = await fetch(
-      `https://git-greeter-server.vashishth-patel.repl.co/api/user/frombot?username=${context.payload.repository.owner.login}`,
+      `${SERVER_URL}username=${context.payload.repository.owner.login}`,
       {
         method: "GET",
       }
     );
     responseData = await response.json();
     console.log(response.status);
-    if (response.status != 201) {
+    if (response.status != 200) {
       console.log("User Not Found IG", responseData);
       return 0;
     }
   } catch (error) {
     console.log("Error in the response", error);
   }
-  return responseData;
+  return responseData[0];
 };
+
+
+module.exports = {
+  getUser
+}
